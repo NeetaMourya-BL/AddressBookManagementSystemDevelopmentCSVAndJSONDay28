27cde0635251502bc345bf094240f78d0b23ec5f
unknown <neetamourya24@gmail.com>
2021-12-28 19:24:37 +0530

    first commit

---
+++ src/main/java/com/bridgelabz/csvAndJson/AddressBookSystem.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.csvAndJson;
+
+/**
+ * Address Book System!
+ *
+ */
+public class AddressBookSystem 
+{
+    public static void main( String[] args )
+    {
+   	 System.out.println("Welcome to Address Book Program");
+    }
+}
---
+++ src/test/java/com/bridgelabz/csvAndJson/AppTest.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.csvAndJson;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
